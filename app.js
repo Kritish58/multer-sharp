621e3a6e28aa63b53b6690ff09be23381a8619ba
kritish bhattarai <rishi58rishi@gmail.com>
2021-01-19 14:23:34 +0545

    second clip

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
 const express = require('express');
 const chalk = require('chalk');
+const multer = require('multer');
+const mkdirp = require('mkdirp');
+const path = require('path');
+const { v4: uuid } = require('uuid');
 const app = express();
 
 const PORT = 3000;
+
+mkdirp.sync(path.join(__dirname, '/uploads'));
+
+const storage = multer.diskStorage({
+    destination: (req, file, done) => {
+        done(null, '/uploads');
+    },
+    filename: (req, file, done) => {
+        done(null, uuid() + '___' + file.originalname);
+    },
+});
+
+const limits = {
+    fileSize: 5 * 1024 * 1024,
+};
+
+const fileFilter = (req, file, done) => {
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+        done(null, true);
+    } else {
+        done(new Error('file type not supported'), false);
+    }
+};
 
 app.listen(PORT, () => {
     console.log(chalk.bold.yellow('server is running on port ', PORT));
